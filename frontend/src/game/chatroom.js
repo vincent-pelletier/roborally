8da379e74f3108b4bf91e655e1b1d561042ecf52
Vincent Pelletier <pelletier.v@hotmail.com>
2021-11-09 22:35:19 -0500

    Chatbox skeleton

--- frontend/src/game/chatroom.js
+++ frontend/src/game/chatroom.js
@@ -4,7 +4,18 @@ const ChatRoom = () => {
 
     return (
         <div className="chat">
-            chat
+            <div className="chat-header">
+                Robo Rally
+            </div>
+            <div className="chatbox">
+                <div className="chatbox-inner">
+                    <span>chatbox</span>
+                    <span>yo</span>
+                </div>
+            </div>
+            <div className="chat-input">
+                input
+            </div>
         </div>
     );
 };
